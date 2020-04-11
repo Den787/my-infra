b8bda7dbb1416c77319d85c15a96559c93e3609b
Denis Novoselov <den787@yandex.ru>
2020-04-11 17:48:19 +0400

    error ex9

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,3 +13,17 @@ description = "Path to the public key used for ssh access"
 variable disk_image {
 description = "Disk image"
 }
+variable app_disk_image {
+description = "Disk image for reddit app"
+default = "reddit-app-base"
+}
+
+variable db_disk_image {
+description = "Disk image for reddit db"
+default = "reddit-db-base"
+}
+
+variable "zone" {
+  description = "Zone"
+  default = "europe-west1-b"
+}
